4dc8dcd57c669a69b9b2f97b97fee5235f1b0a35
Arya Raj <aryaraj152@gmail.com>
2021-07-29 12:46:24 +0530

    Add files via upload

---
+++ React1/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+import App1 from './Example/Example1';
+import Toggle from './Example/Example3';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <App1 />
+        <Toggle />
+      </header>
+    </div>
+  );
+}
+
+export default App;
