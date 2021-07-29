b608650ea7a0bf3b029890b5d0cb041e2800863c
Arya Raj <aryaraj152@gmail.com>
2021-07-29 12:47:19 +0530

    Add files via upload

--- react/App.js
+++ react/App.js
@@ -1,38 +1,25 @@
 import React from 'react';
+import { ReactDOM } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home';
-import About from './components/About';
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
-function App() {
-  return (
-    <Router>
-    <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-
-            <NavLink exact to="/" className="my-link">Home</NavLink>
-            <NavLink exact to="/about" className="my-link">About</NavLink>
-            
-            
-          </div>
-        </div>
-      </nav>
-    </div>
-    <Route exact path="/">
-      <Home />
-    </Route>
-    <Route exact path="/about">
-      <About />
-    </Route>
-    </Router>
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+function tick() {
+  <App />,
+  ReactDOM.render(
+    <Clock date = {new Date()} />,
+    document.getElementById('root')
   );
 }
 
-
+setInterval(tick,1000);
 export default App;
