0fe538afb93daf13e0cfe06ec02dfb59071b2f98
Arya Raj <aryaraj152@gmail.com>
2021-08-15 16:33:56 +0530

    Add files via upload

---
+++ spring-boot/employe-management/src/main/java/com/example/demo/controller/EmpController.java
@@ -0,0 +1,71 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.service.EmpService;
+
+@Controller
+public class EmpController {
+	
+	@Autowired
+	private EmpService service;
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+	
+	@GetMapping("/empform")
+	public String addEmpForm() {
+		return "empform";
+	}
+	
+	@PostMapping("/save")
+	public String empSave(@ModelAttribute Employee e,HttpSession session) {
+		System.out.println(e);
+		service.addEmp(e);
+		session.setAttribute("msg", "Employee added successfully..");
+		return "redirect:/viewemp";
+	}
+	
+	@GetMapping("/viewemp")
+	public String empView(Model m) {
+		List<Employee> emp = service.getAllEmp();
+		m.addAttribute("emp", emp);
+		return "viewemp";
+	}
+	
+	@GetMapping("/empeditform/{id}")
+	public String empEdit(@PathVariable int id,Model m) {
+		
+		Employee e=service.getEmpById(id);
+		m.addAttribute("emp", e);
+		return "empeditform";
+	}
+	
+	@PostMapping("/update")
+	public String empUpdate(@ModelAttribute Employee e,HttpSession session) {
+		
+		service.addEmp(e);
+		session.setAttribute("msg", "Employee updated successfully..");
+		return "redirect:/viewemp";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String empDelete(@PathVariable int id,HttpSession session) {
+		service.deleteEmp(id);
+		session.setAttribute("msg", "Employee deleted successfully..");
+		return "redirect:/viewemp";
+	}
+}
---
+++ spring-boot/employe-management/src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,57 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="EMP_SYM")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;    
+	private String name;    
+	private float salary;
+	private String designation; 
+	private String address;
+	
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
