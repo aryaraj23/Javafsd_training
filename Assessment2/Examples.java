3bbf838c535c584e4a69246696f19aff350c66b9
Arya Raj <aryaraj152@gmail.com>
2021-08-30 10:52:11 +0530

    Add files via upload

---
+++ Assessment2/Examples.java
@@ -0,0 +1,26 @@
+com.ust.example;
+
+
+public class Examples {
+    
+public static void main( String args[] ) {
+        
+String str = "Hello World"; 
+	
+String str1="Hi";
+        
+System.out.println(str.contains("World")); 
+        
+System.out.println(str.contains("world")); 
+ 	
+System.out.println(str.length());
+	
+String new=str.replace('e','o');
+	
+System.out.println(new);  
+	
+String concatt=str.concat(str1);
+	
+System.out.println(str.equals(str1));                           
+    }
+}
---
+++ Assessment2/ReverseString.java
@@ -0,0 +1,19 @@
+com.ust.examples;
+
+
+public class ReverseString {
+    public static void main(String[] args)
+    {
+   	char ch[]=str.toCharArray();  
+    
+	String rev="";  
+    
+	for(int i=ch.length-1;i>=0;i--){  
+        
+		rev+=ch[i];  
+    
+	}  
+    
+	return rev; 
+    }
+}
